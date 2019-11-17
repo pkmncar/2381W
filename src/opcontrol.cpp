d004447bd9fe5904d6f9078bab7fb687f2e89256
unknown <dhao100@gmail.com>
2019-11-17 18:10:48 -0500

    Added some macros, new folder for extra things

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -11,100 +11,162 @@
 #define MOTOR7 5 //LIntake
 #define MOTOR8 14 //RIntake
 
-/*DO WE NEED ENCODERS???
+using namespace pros;
 
-#define ENCODERLT 1
-#define ENCODERLB 2
-#define ENCODERRT 3
-#define ENCODERRB 4
-#define ENCODERCT 5
-#define ENCODERCB 6
+//defines controller
+Controller master (CONTROLLER_MASTER);
+Mutex mutex;
 
-  WILL NEED TO DEFINE THE PORTS FOR THE OTHER MOTORS, SENSORS, ETC.
+//defines the ports that are associated with each wheel
+Motor left_wheels_1 (MOTOR1, 0); //L1
+Motor right_wheels_1 (MOTOR3, 1); //R1
+Motor left_wheels_2 (MOTOR2, 1); //L2
+Motor right_wheels_2 (MOTOR4, 0); //R2
 
-*/
+Motor intake_left (MOTOR7, 0);
+Motor intake_right (MOTOR8, 1);
+Motor arm (MOTOR6, 1);
+Motor angler (MOTOR5, 1);
 
 void drive(void*) {
-	//defines the ports that are associated with each wheel
-	pros::Motor left_wheels_1(MOTOR1, 0); //L1
-	pros::Motor right_wheels_1(MOTOR3, 1); //R1
-	pros::Motor left_wheels_2(MOTOR2, 1); //L2
-	pros::Motor right_wheels_2(MOTOR4, 0); //R2
-
-	//defines controller
-	pros::Controller master(CONTROLLER_MASTER);
 
 	//loop reads the joystick controlls and powers the motors accordingly
-	while (true) {
+	while(true) {
 		int powerLeft = master.get_analog(ANALOG_LEFT_Y);
 		int powerRight = master.get_analog(ANALOG_RIGHT_Y);
 
 		//If there is a small difference, then program will assume person wants to go straight and will set power to motors as equal
-		if ((powerLeft - powerRight) >= -7 && (powerLeft - powerRight) <= 7)
+		if((powerLeft - powerRight) >= -7 && (powerLeft - powerRight) <= 7)
 			powerLeft = powerRight;
 
 		left_wheels_1.move(powerLeft);
 		left_wheels_2.move(powerLeft);
 		right_wheels_1.move(powerRight);
 		right_wheels_2.move(powerRight);
-		pros::delay(20);
+		delay(20);
 	}
 }
 
-void liftingApparatus(void*) {
-	pros::Motor intake_left(MOTOR7, 0);
-	pros::Motor intake_right(MOTOR8, 1);
-	pros::Motor angler(MOTOR5, 1);
-	pros::Motor arm(MOTOR6, 1);
-	//defines controller
-	pros::Controller master(CONTROLLER_MASTER);
+void intake(void*) {
 
 	int intake_power = 100;
-	int angler_power = 100;
-	int arm_power = 100;
+	//moves intakes
+	while(true) {
+	if(master.get_digital(DIGITAL_R2)) {
+		intake_left.move(intake_power);
+		intake_right.move(intake_power);
+	}
+	else if(master.get_digital(DIGITAL_L2)) {
+		intake_left.move(-intake_power);
+		intake_right.move(-intake_power);
+	}
+	else {
+		intake_left.move(0);
+		intake_right.move(0);
+	}
+		delay(20);
+	}
+}
 
-	while (true) {
-		//moves intakes
-		if (master.get_digital(DIGITAL_R2)) {
-			intake_left.move(intake_power);
-			intake_right.move(intake_power);
-		}
-		else if (master.get_digital(DIGITAL_L2)) {
-			intake_left.move(-intake_power);
-			intake_right.move(-intake_power);
-		}
-		else {
-			intake_left.move(0);
-			intake_right.move(0);
-		}
+void arms(void*) {
 
-		//moves angler
-		if (master.get_digital(DIGITAL_R1)) {
-			angler.move(angler_power);
-		}
-		else if (master.get_digital(DIGITAL_L1)) {
-			angler.move(-angler_power);
-		}
-		else {
-			angler.move(0);
-		}
+		int arm_power = 100;
 
-		//moves arm
-		if (master.get_digital(DIGITAL_UP)) {
-			arm.move(arm_power);
-		}
-		else if (master.get_digital(DIGITAL_DOWN)) {
-			arm.move(-arm_power);
-		}
-		else {
-			arm.move(0);
-		}
-
+		while(true) {
+	//moves arm
+	if(master.get_digital(DIGITAL_UP)) {
+		arm.move(arm_power);
+	}
+	else if(master.get_digital(DIGITAL_DOWN)) {
+		arm.move(-arm_power);
+	}
+	else {
+		arm.move(0);
+	}
 		pros::delay(20);
 	}
+
+
+}
+
+void anglerMove(void*) {
+
+	int angler_power = 100;
+
+	while(true) {
+	//moves angler
+	if(master.get_digital(DIGITAL_R1)) {
+		angler.move(angler_power);
+	}
+	else if(master.get_digital(DIGITAL_L1)) {
+		angler.move(-angler_power);
+	}
+	else {
+		angler.move(0);
+	}
+	delay(20);
+	}
+}
+
+void towerScore(void*) {
+
+	if(master.get_digital(DIGITAL_A)) {
+
+	mutex.take(TIMEOUT_MAX);
+
+	arm.set_zero_position(0);
+	arm.move_absolute(100, 100); //change values later to not damage robot!
+	while (!((arm.get_position() < 105) && (arm.get_position() > 95))) {
+    delay(20);
+  }
+
+	intake_left.move_relative(360, -100); //change values later to not damage robot!
+	intake_right.move_relative(360, -100); //change values later to not damage robot!
+	while (!((arm.get_position() < 365) && (arm.get_position() > 355))) {
+    delay(20);
+  }
+
+	arm.move_absolute(0, -100); //change values later to not damage robot!
+	while (!((arm.get_position() < 5) && (arm.get_position() > -5))) {
+    delay(20);
+  }
+
+	mutex.give();
+	}
+
+}
+
+void stacker(void*) {
+
+	if(master.get_digital(DIGITAL_X)) {
+
+	mutex.take(TIMEOUT_MAX);
+
+	angler.set_zero_position(0);
+	angler.move_absolute(100, 100); //change values later to not damage robot!
+	while (!((arm.get_position() < 105) && (arm.get_position() > 95))) {
+		delay(20);
+	}
+
+	left_wheels_1.move_relative(45, 50); //change values later to not damage robot!
+	left_wheels_2.move_relative(45, 50); //change values later to not damage robot!
+	right_wheels_1.move_relative(45, 50); //change values later to not damage robot!
+	right_wheels_2.move_relative(45, 50); //change values later to not damage robot!
+	while (!((arm.get_position() < 365) && (arm.get_position() > 355))) {
+		delay(20);
+	}
+
+	mutex.give();
+	}
+
+
 }
 
 void opcontrol() {
-	pros::Task task1(drive, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Driving");
-	pros::Task task2(liftingApparatus, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Lifting");
+	Task task1 (drive, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Driving");
+	Task task2 (intake, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Intake");
+	Task task3 (anglerMove, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Angler");
+	Task task4 (arms, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Arms");
+	Task task5 (towerScore, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Tower scoring");
+	Task task6 (stacker, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Cube stacking");
 }
